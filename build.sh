1e528722ed5bbf81fa12fc3d235dd043db62d714
suraj-goodpatch <suraj.sau@goodpatch.com>
2020-05-10 11:04:13 +0530

    Added build script to install application

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_LOCATION="./script"
+OUTPUT_LOCATION="./build"
+SCRIPT_FILE="password.scpt"
+ICON_FILE="applet.icns"
+
+APP_NAME="MyPass"
+
+mkdir -p $OUTPUT_LOCATION
+
+osacompile -o "$OUTPUT_LOCATION/$APP_NAME.app" "$SCRIPT_LOCATION/$SCRIPT_FILE"
+
+rm "$OUTPUT_LOCATION/$APP_NAME.app/Contents/Resources/applet.icns"
+cp "$SCRIPT_LOCATION/$ICON_FILE" "$OUTPUT_LOCATION/$APP_NAME.app/Contents/Resources/applet.icns"
+
+echo "Build: $OUTPUT_LOCATION/APP_NAME.app"
