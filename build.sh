6e435824230facf54fa5e915c98ec6d89daecefd
suraj <i@surajsau.in>
2020-05-15 17:50:10 +0530

    Fixed password entry script into text file

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 SCRIPT_LOCATION="./script"
 OUTPUT_LOCATION="./build"
 APP_NAME="MyPass"
+SCRIPT_APP="script.app"
 ICON_FILE="applet.icns"
 
 APP_NAME="MyPass"
@@ -11,12 +12,13 @@ mkdir -p $OUTPUT_LOCATION
 rm -rf $OUTPUT_LOCATION/*
 mkdir -p $OUTPUT_LOCATION/$APP_NAME.app
 
-cp -R $SCRIPT_LOCATION/$APP_NAME.app/* "$OUTPUT_LOCATION/$APP_NAME.app/"
+cp -R $SCRIPT_LOCATION/$SCRIPT_APP/* "$OUTPUT_LOCATION/$APP_NAME.app/"
 
 rm "$OUTPUT_LOCATION/$APP_NAME.app/Contents/Resources/applet.icns"
 cp "$SCRIPT_LOCATION/$ICON_FILE" "$OUTPUT_LOCATION/$APP_NAME.app/Contents/Resources/applet.icns"
 
 echo "Build: $OUTPUT_LOCATION/$APP_NAME.app"
 
+rm -rf "/Applications/$APP_NAME.app"
 mv "$OUTPUT_LOCATION/$APP_NAME.app" "/Applications/"
 echo "Moved to Applications"
